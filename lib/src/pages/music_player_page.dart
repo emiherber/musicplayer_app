c35e259e1ff776eb32155da7fb144f15ff861578
Emiliano Herber <emiherbes@hotmail.com>
2022-05-14 01:15:34 -0300

    custom app bar

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:musicplayer_app/src/widgets/custom_appbar.dart';
 
 class MusicPlayerPage extends StatelessWidget {
   const MusicPlayerPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          CustomAppBar(),
+        ],
+      ),
+    );
   }
 }
